015322f6d1c29df7c03ef48b97152b1df312ab73
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-26 01:23:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+def begins_with_r(array)
+  array.all?  {|word| word.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|element| element.include?('a')}
+end
+
+def first_wa(array)
+  array.find {|element| element.start_with?("wa")}
+  array.find
+end
+
+def remove_non_strings(array)
+  array.delete_if {|element| element.class != String}
+end
+
+def count_elements(array)
+  array
+end
+
+def merge_data(keys, data)
+  
+end
+
+def find_cool(array)
+  array.select {|element| element.include?('cool')}
+end
+
+def organize_schools(schools)
+  location.sort
+end
